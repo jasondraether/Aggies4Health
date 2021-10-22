dd53448bf13743abef7e16df04cde9ae676370ce
JRaether16 <jasondraether@gmail.com>
2021-10-22 15:23:48 -0500

    updated counts

--- scripts/bash_scripts/counts.sh
+++ scripts/bash_scripts/counts.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-echo "Counting JSON objects..."
+
+#for i in {01..16};
+#do
+#	for f in "../../data/pmdata/p${i}/fitbit/"*.json;
+#	do
+#		echo -n "${f}: "
+#		jq length "${f}"
+#	done
+#done
 
 for i in {01..16};
 do
-	for f in "../../data/pmdata/p${i}/fitbit/"*.json;
-	do
-		echo -n "${f}: "
-		jq length "${f}"
-	done
-done
-
-echo "Counting CSV lines..."
-
-for i in {01..16};
-do
-        for f in "../../data/pmdata/p${i}/fitbit/"*.json;
+        for f in "../../../data/pmdata/p${i}/googledocs/"*.csv;
         do
                 wc -l "${f}"
         done
 done
 
-echo "Finished"
 
--- scripts/python_scripts/counts_to_excel.py
+++ scripts/python_scripts/counts_to_excel.py
@@ -47,5 +47,17 @@ with open(os.path.join(path_to_files, 'json_counts_fitbit.txt'), 'r') as f:
         df_dict['type'].append('.csv')
 
 
+with open(os.path.join(path_to_files, 'reporting_counts.txt'), 'r') as f:
+    for line in f.readlines():
+        count, fp_raw = line.strip().split(' ')
+        _,_,_,_,_, pt, src, fname = fp_raw.split('/')
+
+        df_dict['participant'].append(pt)
+        df_dict['file_name'].append(fname)
+        df_dict['source'].append(src)
+        df_dict['count'].append(int(count))
+        df_dict['type'].append('.csv')
+        
+
 df = pd.DataFrame(df_dict)
 df.to_excel('counts_formatted.xlsx', index=False, header=True)
